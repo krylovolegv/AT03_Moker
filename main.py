fe41bbfd5bbbea27683b5abdca703a4bf193376b
krylovolegv <krylovolegv@yandex.ru>
2024-07-25 15:31:07 +0200

    test 2

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import requests
-
-def get_weather(api_key, city):
-   url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+def get_github_user(username):
+   url = f'https://api.github.com/users/{username}'
    response = requests.get(url)
    if response.status_code == 200:
        return response.json()
    else:
        return None
+
+# def get_weather(api_key, city):
+#    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+#    response = requests.get(url)
+#    if response.status_code == 200:
+#        return response.json()
+#    else:
+#        return None
--- test.py
+++ test.py
@@ -1,34 +1,59 @@
 import pytest
-from main import get_weather  # замените my_module на имя вашего модуля
+from main import get_github_user
 
-
-def test_get_weather_success(mocker):
+def test_get_github_user(mocker):
    mock_get = mocker.patch('main.requests.get')
-
-   # Создаем мок-ответ для успешного запроса
    mock_get.return_value.status_code = 200
    mock_get.return_value.json.return_value = {
-       'weather': [{'description': 'clear sky'}],
-       'main': {'temp': 282.55}
+       'login' : 'nizavr',
+       'id' : 345178,
+       'name' : 'Nina'
    }
 
-   api_key = 'test_api_key'
-   city = 'London'
-   weather_data = get_weather(api_key, city)
-
-   assert weather_data == {
-       'weather': [{'description': 'clear sky'}],
-       'main': {'temp': 282.55}
+   user_data = get_github_user('cat')
+   assert user_data == {
+       'login' : 'nizavr',
+       'id' : 345178,
+       'name' : 'Nina'
    }
 
-def test_get_weather_failure(mocker):
+def test_get_github_user_with_error(mocker):
    mock_get = mocker.patch('main.requests.get')
+   mock_get.return_value.status_code = 500
 
-   # Создаем мок-ответ для неуспешного запроса
-   mock_get.return_value.status_code = 404
+   user_data = get_github_user('cat')
+   assert user_data == None
 
-   api_key = 'test_api_key'
-   city = 'UnknownCity'
-   weather_data = get_weather(api_key, city)
-
-   assert weather_data is None
+# from main import get_weather  # замените my_module на имя вашего модуля
+#
+#
+# def test_get_weather_success(mocker):
+#    mock_get = mocker.patch('main.requests.get')
+#
+#    # Создаем мок-ответ для успешного запроса
+#    mock_get.return_value.status_code = 200
+#    mock_get.return_value.json.return_value = {
+#        'weather': [{'description': 'clear sky'}],
+#        'main': {'temp': 282.55}
+#    }
+#
+#    api_key = 'test_api_key'
+#    city = 'London'
+#    weather_data = get_weather(api_key, city)
+#
+#    assert weather_data == {
+#        'weather': [{'description': 'clear sky'}],
+#        'main': {'temp': 282.55}
+#    }
+#
+# def test_get_weather_failure(mocker):
+#    mock_get = mocker.patch('main.requests.get')
+#
+#    # Создаем мок-ответ для неуспешного запроса
+#    mock_get.return_value.status_code = 404
+#
+#    api_key = 'test_api_key'
+#    city = 'UnknownCity'
+#    weather_data = get_weather(api_key, city)
+#
+#    assert weather_data is None
